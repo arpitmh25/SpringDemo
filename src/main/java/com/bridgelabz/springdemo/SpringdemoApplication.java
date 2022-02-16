7d2276aed2c5fa5173020f4e474694a126a48ae8
Arpit <arpitmh23@gmail.com>
2022-02-16 20:08:53 +0530

    Creating app to display hello message

---
+++ src/main/java/com/bridgelabz/springdemo/SpringdemoApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello From Bridgelabz..!");
+        SpringApplication.run(SpringdemoApplication.class, args);
+    }
+
+}
